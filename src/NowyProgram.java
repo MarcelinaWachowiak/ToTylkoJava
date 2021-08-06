452fd05131486e7a5cb2c631b2cccfbc5d63f147
Marcelina Wachowiak <87701519+MarcelinaWachowiak@users.noreply.github.com>
2021-08-06 22:54:27 +0200

    Add files via upload
    pierwsze zadanie

---
+++ src/NowyProgram.java
@@ -0,0 +1,13 @@
+public class NowyProgram {
+    public static void main(String[] args) {
+        int humanAge = 10;
+
+        if (humanAge > 50) {
+            System.out.println("Czlowiek jest stary");
+        } else if (humanAge < 20) {
+            System.out.println("Czlowiek jest mlody");
+        } else {
+            System.out.println("Czlowiek jest w srednim wieku");
+        }
+    }
+}
